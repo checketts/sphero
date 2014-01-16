51a849f2d05093ca9a1e4d6e94b6917483d77ecd
Gregary Allen Hendricks <gregaryh@lapras.local>
2014-01-15 20:08:41 -0700

    connect to shpero PRO

--- color-moving-and-api.js
+++ color-moving-and-api.js
@@ -1,7 +1,7 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-    connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-RRG-AMP-SPP' },
+    connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-PRO-RN-SPP' },
     device: { name: 'sphero', driver: 'sphero' },
 
     work: function(my) {
